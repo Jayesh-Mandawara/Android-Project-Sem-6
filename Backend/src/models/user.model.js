5a4a1dd4e74f9ebc78362ce17e8874b974f1ca32
Jayesh Mandawara <jaymandawara08@gmail.com>
2026-04-27 23:34:08 +0530

    Removed and resolved coming while registering the user as any of the role

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -28,11 +28,10 @@ const userSchema = new mongoose.Schema({
     passwordChangedAt: Date,
 });
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
 
     this.password = await bcrypt.hash(this.password, 12);
-    next();
 });
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
